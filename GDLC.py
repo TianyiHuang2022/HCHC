ef04d3c6e9a41e44b1fb2bbbfa3977ffc30bf8de
TianyiHuang2022 <107926026+TianyiHuang2022@users.noreply.github.com>
2023-06-16 22:19:26 +0800

    Add files via upload

---
+++ GDLC.py
@@ -0,0 +1,316 @@
+from __future__ import print_function, division
+import argparse
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics.cluster import normalized_mutual_info_score as nmi_score
+from sklearn.metrics import adjusted_rand_score as ari_score
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.parameter import Parameter
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torch.nn import Linear
+from utils import MnistDataset
+
+from utils_algo import PairEnum, BCE_softlabels, cluster_acc
+
+
+class AE(nn.Module):
+
+    def __init__(self, n_enc_1, n_enc_2, n_enc_3, n_dec_1, n_dec_2, n_dec_3,
+                 n_input, n_z):
+        super(AE, self).__init__()
+
+        # encoder
+        self.enc_1 = Linear(n_input, n_enc_1)
+        self.enc_2 = Linear(n_enc_1, n_enc_2)
+        self.enc_3 = Linear(n_enc_2, n_enc_3)
+
+        self.z_layer = Linear(n_enc_3, n_z)
+
+        # decoder
+        self.dec_1 = Linear(n_z, n_dec_1)
+        self.dec_2 = Linear(n_dec_1, n_dec_2)
+        self.dec_3 = Linear(n_dec_2, n_dec_3)
+
+        self.x_bar_layer = Linear(n_dec_3, n_input)
+
+    def forward(self, x):
+
+        # encoder
+        enc_h1 = F.relu(self.enc_1(x))
+        enc_h2 = F.relu(self.enc_2(enc_h1))
+        enc_h3 = F.relu(self.enc_3(enc_h2))
+
+        z = self.z_layer(enc_h3)
+
+        # decoder
+        dec_h1 = F.relu(self.dec_1(z))
+        dec_h2 = F.relu(self.dec_2(dec_h1))
+        dec_h3 = F.relu(self.dec_3(dec_h2))
+        x_bar = self.x_bar_layer(dec_h3)
+
+        return x_bar, z
+
+
+class IDEC(nn.Module):
+
+    def __init__(self,
+                 n_enc_1,
+                 n_enc_2,
+                 n_enc_3,
+                 n_dec_1,
+                 n_dec_2,
+                 n_dec_3,
+                 n_input,
+                 n_z,
+                 n_clusters,
+                 alpha=1,
+                 pretrain_path='data/ae_mnist.pkl'):
+        super(IDEC, self).__init__()
+        self.alpha = 1.0
+        self.pretrain_path = pretrain_path
+
+        self.ae = AE(
+            n_enc_1=n_enc_1,
+            n_enc_2=n_enc_2,
+            n_enc_3=n_enc_3,
+            n_dec_1=n_dec_1,
+            n_dec_2=n_dec_2,
+            n_dec_3=n_dec_3,
+            n_input=n_input,
+            n_z=n_z)
+        # cluster layer
+        self.cluster_layer = Parameter(torch.Tensor(n_clusters, n_z))
+        torch.nn.init.xavier_normal_(self.cluster_layer.data)
+
+    def pretrain(self, path=''):
+        if path == '':
+            pretrain_ae(self.ae)
+        # load pretrain weights
+        self.ae.load_state_dict(torch.load(self.pretrain_path))
+        print('load pretrained ae from', path)
+
+    def forward(self, x):
+
+        x_bar, z = self.ae(x)
+        # cluster
+        q = 1.0 / (1.0 + torch.sum(
+            torch.pow(z.unsqueeze(1) - self.cluster_layer, 2), 2) / self.alpha)
+        q = q.pow((self.alpha + 1.0) / 2.0)
+        q = (q.t() / torch.sum(q, 1)).t()
+        center = self.cluster_layer
+        return x_bar, q, center
+
+
+
+def add_noise(img):
+	noise = torch.randn(img.size()) * args.epsilon
+	noisy_img = img + noise
+	return noisy_img, noise
+
+def target_distribution(q):
+    weight = q**2 / q.sum(0)
+    return (weight.t() / weight.sum(1)).t()
+
+def CKROD(Dist, sigma): # An effective keneral rank-order similarity measure
+    Dist = Dist.cpu()
+    Dist = Dist / Dist.max()
+    Rank = Dist.argsort().argsort()
+    Rdist = Rank + Rank.t() + 1
+    KROD = Rdist.float() * torch.exp(Dist / sigma)
+    return KROD
+
+
+def pretrain_ae(model):
+    '''
+    pretrain autoencoder
+    '''
+    train_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+    print(model)
+    optimizer = Adam(model.parameters(), lr=0.002)
+    pretrain = args.pretrain
+    if pretrain:
+        n_p = 30
+    else:
+        n_p = 0
+    for epoch in range(n_p):
+        total_loss = 0.
+        for batch_idx, (x, _, _) in enumerate(train_loader):
+            x = x.to(device)
+            optimizer.zero_grad()
+            x_bar, z = model(x)
+            loss = F.mse_loss(x_bar, x)
+            total_loss += loss.item()
+
+            loss.backward()
+            optimizer.step()
+
+        print("epoch {} loss={:.4f}".format(epoch,
+                                            total_loss / (batch_idx + 1)))
+        torch.save(model.state_dict(), args.pretrain_path)
+    print("model saved to {}.".format(args.pretrain_path))
+
+
+
+def train_idec():
+
+    model = IDEC(
+        n_enc_1=500,
+        n_enc_2=500,
+        n_enc_3=2000,
+        n_dec_1=2000,
+        n_dec_2=500,
+        n_dec_3=500,
+        n_input=args.n_input,
+        n_z=args.n_z,
+        n_clusters=args.n_clusters,
+        alpha=1.0,
+        pretrain_path=args.pretrain_path).to(device)
+
+    #  model.pretrain('data/ae_mnist.pkl')
+    model.pretrain()
+
+
+
+
+
+    bce = BCE_softlabels()
+
+    train_loader = DataLoader(
+        dataset, batch_size=args.batch_size, shuffle=True, drop_last=True, num_workers=2)
+
+    optimizer = Adam(model.parameters(), lr=args.lr, betas=(0.9, 0.99))
+
+    # cluster parameter initiate
+    data = dataset.x
+    y = dataset.y
+
+
+
+    data = torch.Tensor(data).to(device)
+    x_bar, hidden = model.ae(data)
+
+    kmeans = KMeans(n_clusters=args.n_clusters, random_state=0)
+    y_pred = kmeans.fit_predict(hidden.data.cpu().numpy())
+    nmi_k = nmi_score(y_pred, y)
+    print("nmi score={:.4f}".format(nmi_k))
+
+
+    y_pred_last = y_pred
+    model.cluster_layer.data = torch.tensor(kmeans.cluster_centers_).to(device)
+
+    model.train()
+    n_neighbors = args.n_neighbors
+    bate1 = args.beta1
+    bate2 = args.beta2
+    gamma = args.gamma_l
+    for epoch in range(200):
+
+        if epoch % args.update_interval == 0:
+
+            _, tmp_q,_ = model(data)
+
+            # update target distribution p
+            tmp_q = tmp_q.data
+            p = target_distribution(tmp_q)
+
+            # evaluate clustering performance
+            y_pred = tmp_q.cpu().numpy().argmax(1)
+            delta_label = np.sum(y_pred != y_pred_last).astype(
+                np.float32) / y_pred.shape[0]
+            y_pred_last = y_pred
+
+            acc = cluster_acc(y, y_pred)
+            nmi = nmi_score(y, y_pred)
+            ari = ari_score(y, y_pred)
+            print('Iter {}'.format(epoch), ':Acc {:.4f}'.format(acc),
+                  ', nmi {:.4f}'.format(nmi), ', ari {:.4f}'.format(ari))
+
+
+        for batch_idx, (x, _, idx) in enumerate(train_loader):
+
+
+
+            xn, added_noise = add_noise(x)
+            xn = xn.to(device)
+            x = x.to(device)
+
+            x_bar, q, _ = model(x)
+            xn_bar, qn, _ = model(xn)
+            _, hidden = model.ae(x)
+
+
+
+            feat_detach = hidden.detach()
+            feat_row, feat_col = PairEnum(feat_detach)
+            tmp_distance_ori = ((feat_row - feat_col) ** 2.).sum(1).view(x.size(0), x.size(0))
+            tmp_distance_ori = CKROD(tmp_distance_ori, 20)
+            tmp_distance_ori = tmp_distance_ori.cpu().float()
+            target_ulb = torch.zeros_like(tmp_distance_ori).float()
+            target_ulb[tmp_distance_ori < torch.kthvalue(tmp_distance_ori, n_neighbors, 0, True)[0]] = 1
+            target_ulb = target_ulb.mul(torch.exp(-0.1*tmp_distance_ori))
+            target_ulb = target_ulb.view(-1)
+
+            prob_bottleneck_row, prob_bottleneck_col = PairEnum(q)
+            m_loss = bce(prob_bottleneck_row, prob_bottleneck_col, target_ulb)
+            m_loss = max(bate1*gamma**epoch, 0.8)*m_loss
+            m_loss = m_loss.cuda()
+
+
+            reconstr_loss = F.mse_loss(x_bar, x)
+            lossc = F.mse_loss(qn, q)
+            idx = idx.long()
+            kl_loss = F.kl_div(q.log(), p[idx])
+            loss = reconstr_loss + m_loss + lossc * bate2 +0.001*kl_loss
+
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    output_f = tmp_q.cpu().numpy()
+    np.save('embedded_MNIST'+str(epoch)+'.npy', output_f)
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description='train',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--lr', type=float, default=0.002)
+    parser.add_argument('--pretrain', type=bool, default=False)
+    parser.add_argument('--n_clusters', default=10, type=int)
+    parser.add_argument('--batch_size', default=128, type=int)
+    parser.add_argument('--n_z', default=5, type=int)
+    parser.add_argument('--dataset', type=str, default='mnist')
+    parser.add_argument('--pretrain_path', type=str, default='data/ae_mnist.pkl')
+    parser.add_argument(
+        '--gamma',
+        default=0.1,
+        type=float,
+        help='coefficient of clustering loss')
+    parser.add_argument('--update_interval', default=1, type=int)
+    parser.add_argument('--tol', default=0.001, type=float)
+    parser.add_argument('--n_neighbors', default=11, type=float)
+    parser.add_argument('--beta1', default=5, type=float)
+    parser.add_argument('--gamma_l', default=0.8, type=float)
+    parser.add_argument('--beta2', default=10, type=float)
+    parser.add_argument('--epsilon', default=0.25, type=float)
+
+    args = parser.parse_args()
+    args.cuda = torch.cuda.is_available()
+    print("use cuda: {}".format(args.cuda))
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    args.n_clusters = 10
+    args.n_input = 784
+    dataset = MnistDataset()
+    Y = dataset.y
+    np.save('Mnist_y.npy', Y)
+
+    print(args)
+    train_idec()
---
+++ HCHC_mapping.py
@@ -0,0 +1,145 @@
+import numpy as np
+import math
+from pandas import DataFrame
+import matplotlib.pyplot as pyplot
+from utils import MnistDataset
+
+
+
+
+
+
+
+
+
+
+data = np.load('embedded_MNIST.npy')
+dataset = MnistDataset()
+Y = dataset.y
+nx = len(Y)
+n_clu = len(np.unique(Y))
+
+corr = np.corrcoef(data.T)
+vertex = (1-corr)/2
+
+def DGBC(data):
+    m, n = data.shape[0], data.shape[1]
+    corr = np.corrcoef(data.T)
+    vertex = (1-corr)/2
+    dimorder = getShortestHamiltonianCycle(vertex)
+    dimorder = shift(dimorder, 7)
+    dimorder = dimorder.astype(np.int)
+    sumcorr = 0
+    corr = np.zeros(n)
+    for i in range(n-1):
+        corr[i] = vertex[dimorder[i], dimorder[i + 1]]
+        sumcorr = sumcorr + vertex[dimorder[i], dimorder[i + 1]]
+
+    corr[n - 1] = vertex[dimorder[n - 1], dimorder[0]]
+    sumcorr = sumcorr + vertex[dimorder[n - 1], dimorder[0]]
+    angle = np.zeros(n)
+    angle[0] = -math.pi / 2
+    GBCLocation = np.zeros((m + n, 2))
+    GBCLocation[m, 0] = math.cos(angle[0])
+    GBCLocation[m, 1] = math.sin(angle[0])
+
+    for i in range(1, n):
+        angle[i] = angle[i - 1] + 2 * math.pi * (corr[i - 1] / sumcorr)
+        GBCLocation[i + m, 0] = math.cos(angle[i])
+        GBCLocation[i + m, 1] = math.sin(angle[i])
+
+    for i in range(m):
+        tempsum = 0
+        for j in range(n):
+            tempsum = tempsum + data[i, dimorder[j]]
+            GBCLocation[i, 0] = 0
+            GBCLocation[i, 1] = 0
+
+            if tempsum == 0:
+                GBCLocation[i, 0] = 0
+                GBCLocation[i, 1] = 0
+            else:
+                for j in range(n):
+                    GBCLocation[i, 0] = GBCLocation[i, 0] + (data[i, dimorder[j]] / tempsum) * GBCLocation[m + j, 0]
+                    GBCLocation[i, 1] = GBCLocation[i, 1] + (data[i, dimorder[j]] / tempsum) * GBCLocation[m + j, 1]
+
+    return GBCLocation, dimorder
+
+def getShortestHamiltonianCycle(dist):
+    n = len(dist)
+    l = 1 << n
+    dp = np.ones((l, n)) * float('Inf')
+    dp[1][0] = 0
+    for mask in range(1, l, 2):
+        for i in range(1, n, 1):
+            if ((mask & 1 << i) != 0):
+                for j in range(0, n, 1):
+                    if ((mask & 1 << j) != 0):
+                        dp[mask][i] = min(dp[mask][i], dp[mask ^ (1 << i)][j] + dist[j, i])
+
+
+    res = float('Inf')
+    for i in range(1, n, 1):
+        res = min(res, dp[(1 << n) - 1][i] + dist[i, 0])
+    cur = (1 << n) - 1
+    last = 0
+    order = np.zeros(n)
+    for i in range(n-1, 0, -1):
+        bj = 1
+        for j in range(1, n):
+            if ((cur & 1 << j) != 0 and (dp[cur][bj] + dist[bj, last] > dp[cur][j] + dist[j, last])):
+                bj = j
+        order[i] = bj
+        cur ^= 1 << bj
+        last = bj
+        #order = [3,7,6,0,1,8,4,9,5,2]
+    return order
+
+def shift(order, num):
+    lent = len(order)
+    copyorder =  np.zeros(lent)
+    for i in range(lent):
+        copyorder[i]=order[(i + num) % (lent)]
+
+    return copyorder
+
+
+
+
+GBCLocation, order = DGBC(data)
+pyplot.rcParams['savefig.dpi'] = 600
+aa = GBCLocation[:,0]
+bb = GBCLocation[:,1]
+a = GBCLocation[0:nx,0]
+b = GBCLocation[0:nx,1]
+ax = GBCLocation[nx:nx+n_clu,0]
+bx = GBCLocation[nx:nx+n_clu,1]
+
+
+theta = np.linspace(0, 2 * np.pi, 200)
+
+
+
+
+x = np.cos(theta)
+y = np.sin(theta)
+
+
+
+
+
+df = DataFrame(dict(x=a, y=b, label=Y))
+colors = {0: 'grey', 1: 'blue', 2: 'green',  3: 'purple', 4: 'cyan', 5: 'olive', 6: 'darkblue', 7: 'deeppink',  8: 'orange',9: 'darkcyan'}
+fig, axx = pyplot.subplots()
+grouped = df.groupby('label')
+for key, group in grouped:
+    group.plot(ax=axx, kind='scatter', s=0.002, x='x', y='y', marker='h', color=colors[key])
+pyplot.scatter(ax, bx, c="#d62728", alpha=1, s=60, linewidths=1, zorder=1, edgecolors="white")
+pyplot.plot(x, y, c="#d62728", linewidth=1.5, linestyle="--", zorder=0)
+pyplot.axis('equal')
+pyplot.axis('off')
+
+fig.set_size_inches(5, 5)
+pyplot.savefig('MNIST.jpg',bbox_inches='tight', dpi=300, pad_inches=0)
+pyplot.show()
+
