fae1e7ccaec0b5dece4183a230d33a5808d65508
Tianyi Huang <107926026+TianyiHuang2022@users.noreply.github.com>
2023-06-19 21:26:31 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -32,12 +32,6 @@ def load_mnist(path='./data/mnist.npz'):
 
 
 
-def load_fashion(path='./data/fashion.npz'):
-    data = np.load(path)
-    x = torch.Tensor(data["arr_0"] / 255)
-    y = data["arr_1"]
-    return x, y
-
 class MnistDataset(Dataset):
 
     def __init__(self):
@@ -50,18 +44,6 @@ class MnistDataset(Dataset):
         return torch.from_numpy(np.array(self.x[idx])), torch.from_numpy(
             np.array(self.y[idx])), torch.from_numpy(np.array(idx))
 
-
-class fashionDataset(Dataset):
-
-    def __init__(self):
-        self.x, self.y = load_fashion()
-
-    def __len__(self):
-        return self.x.shape[0]
-
-    def __getitem__(self, idx):
-        return torch.from_numpy(np.array(self.x[idx])), torch.from_numpy(
-            np.array(self.y[idx])), torch.from_numpy(np.array(idx))
 
 
 
